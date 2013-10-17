31b5cf848dfba524ab54319a6ca7412f4725c1af
Samuel Vasko <samvasko@gmail.com>
2013-10-17 16:43:43 +0200

    Cleanp for joining images

--- join.go
+++ join.go
@@ -6,53 +6,59 @@ import (
 )
 
 func Join(tiles matrix) {
-	// for dy := tiles.BL.y; dy < tiles.TL.y; dy++ {
-
-	// }
 	imagick.Initialize()
 	defer imagick.Terminate()
 
 	for i := tiles.TL.x; i < tiles.TR.x; i++ {
 		col(tiles.BL.y, tiles.TL.y, i)
 	}
+
 	row(tiles.TL.x, tiles.TR.x)
 }
 
 func row(from, to coord) {
-	base := imagick.NewMagickWand()
-	base.SetImageCompressionQuality(100)
+	base := newBase()
 	defer base.Destroy()
 
 	for i := from; i < to; i++ {
-		additive := imagick.NewMagickWand()
-		filename := fmt.Sprintf("%d.png", i)
-		err := additive.ReadImage(filename)
-		handle(err)
-
-		base.AddImage(additive)
-		additive.Destroy()
+		addImage(base, fmt.Sprintf("%d.png", i))
 	}
 
-	wand := base.MontageImage(imagick.NewDrawingWand(), "5x5", "", imagick.MONTAGE_MODE_CONCATENATE, "0x0")
+	wand := base.MontageImage(imagick.NewDrawingWand(), "x1", "", imagick.MONTAGE_MODE_CONCATENATE, "0x0")
 	wand.WriteImage("complete.png")
 
 }
 
 func col(from, to, x coord) {
-	base := imagick.NewMagickWand()
-	base.SetImageCompressionQuality(100)
+	base := newBase()
 	defer base.Destroy()
 
 	for i := from; i < to; i++ {
-		additive := imagick.NewMagickWand()
-		filename := fmt.Sprintf("%d_%d.png", x, i)
-		err := additive.ReadImage(filename)
-		handle(err)
-
-		base.AddImage(additive)
-		additive.Destroy()
+		addImage(base, fmt.Sprintf("%d_%d.png", x, i))
 	}
 
 	wand := base.MontageImage(imagick.NewDrawingWand(), "1x", "", imagick.MONTAGE_MODE_CONCATENATE, "0x0")
 	wand.WriteImage(fmt.Sprintf("%d.png", x))
+}
+
+/**
+ * Add image to base wand, I know there is way to
+ * add them more at time, but that would constitute
+ * making an array
+ */
+func addImage(base *imagick.MagickWand, filename string) {
+	additive := imagick.NewMagickWand()
+	err := additive.ReadImage(filename)
+	handle(err)
+	defer additive.Destroy()
+	base.AddImage(additive)
+}
+
+/**
+ * Common place for config
+ */
+func newBase() *imagick.MagickWand {
+	base := imagick.NewMagickWand()
+	base.SetImageCompressionQuality(100)
+	return base
 }
