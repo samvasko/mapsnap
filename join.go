b881d0ad41f992314dd9c6991a093f15c1978087
Samuel Vasko <samvasko@gmail.com>
2013-11-07 15:45:11 +0100

    joining using go instead of magick

--- join.go
+++ join.go
@@ -2,64 +2,93 @@ package main
 
 import (
 	"fmt"
-	"github.com/gographics/imagick/imagick"
+	"github.com/nfnt/resize"
+	"image"
+	"image/png"
+	"os"
 )
 
-func Join(tiles matrix) *imagick.MagickWand {
-	imagick.Initialize()
-	defer imagick.Terminate()
+func Join(tiles matrix) image.Image {
+	cols := []string{}
 
-	for i := tiles.TL.x; i < tiles.TR.x; i++ {
-		col(tiles.BL.y, tiles.TL.y, i)
+	// Itinerate over Xes
+	for dx := tiles.TL.x; dx < tiles.TR.x; dx++ {
+		col := []string{}
+		// All y for this x
+		for dy := tiles.BL.y; dy < tiles.TL.y; dy++ {
+			col = append(col, fmt.Sprintf("tile_%d:%d.png", dx, dy))
+		}
+		img := joinByName(col, true)
+		filename := fmt.Sprintf("column_%d.png", dx)
+		fmt.Print("Created: ", filename, "                   \r")
+		Save(img, filename)
+		cols = append(cols, filename)
 	}
 
-	return row(tiles.TL.x, tiles.TR.x)
+	return joinByName(cols, false)
 }
 
-func row(from, to coord) *imagick.MagickWand {
-	base := newBase()
-	defer base.Destroy()
+func Save(img image.Image, filename string) {
+	outfile, err := os.Create(filename)
+	handle(err)
+	png.Encode(outfile, img)
+	outfile.Close()
+}
 
-	for i := from; i < to; i++ {
-		addImage(base, fmt.Sprintf("%d.png", i))
+func joinByName(filenames []string, vertical bool) image.Image {
+
+	var output *image.RGBA
+	var tile_size struct{ dx, dy int }
+	var offset int
+
+	for i, fname := range filenames {
+		tile := open(fname)
+		if i == 0 {
+			offset = 0
+			tile_size.dx = tile.Bounds().Dx()
+			tile_size.dy = tile.Bounds().Dy()
+
+			if vertical {
+				output = createHost(tile_size.dx, tile_size.dy*len(filenames))
+			} else {
+				output = createHost(tile_size.dx*len(filenames), tile_size.dy)
+			}
+		}
+
+		for dx := 0; dx < tile_size.dx; dx++ {
+			for dy := 0; dy < tile_size.dy; dy++ {
+				if vertical {
+					output.Set(dx, dy+(offset*tile_size.dy), tile.At(dx, dy))
+				} else {
+					output.Set(dx+(offset*tile_size.dx), dy, tile.At(dx, dy))
+				}
+			}
+		}
+
+		offset++
 	}
 
-	wand := base.MontageImage(imagick.NewDrawingWand(), "x1", "", imagick.MONTAGE_MODE_CONCATENATE, "0x0")
-	return wand
+	return output
 }
 
-func col(from, to, x coord) {
-	base := newBase()
-	defer base.Destroy()
-
-	for i := from; i < to; i++ {
-		addImage(base, fmt.Sprintf("tile_%d:%d.png", x, i))
-	}
-
-	wand := base.MontageImage(imagick.NewDrawingWand(), "1x", "", imagick.MONTAGE_MODE_CONCATENATE, "0x0")
-	wand.WriteImage(fmt.Sprintf("%d.png", x))
+func createHost(xb, yb int) *image.RGBA {
+	return image.NewRGBA(image.Rect(0, 0, xb, yb))
 }
 
-/**
- * Add image to base wand, I know there is way to
- * add them more at time, but that would constitute
- * making an array
- */
-func addImage(base *imagick.MagickWand, filename string) {
-	additive := imagick.NewMagickWand()
-	err := additive.ReadImage(filename)
+func open(filename string) image.Image {
+	file, err := os.Open(filename)
+	handle(err)
+	img, _, err := image.Decode(file)
+	handle(err)
+
+	file.Close()
+
+	img = resize.Resize(0, 150, img, resize.NearestNeighbor)
+	return img
+}
+
+func handle(err error) {
 	if err != nil {
 		panic(err)
 	}
-	defer additive.Destroy()
-	base.AddImage(additive)
-}
-
-/**
- * Common place for config
- */
-func newBase() *imagick.MagickWand {
-	base := imagick.NewMagickWand()
-	base.SetImageCompressionQuality(100)
-	return base
 }
